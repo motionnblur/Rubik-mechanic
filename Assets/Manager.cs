7d8856a4643b57d71098623e4b2898c5a02e6a83
chill <carbonfiberdev@gmail.com>
2024-04-03 15:41:51 +0300

    add
    added raycast

--- Assets/Manager.cs
+++ Assets/Manager.cs
@@ -8,6 +8,12 @@ public class Manager : MonoBehaviour
     }
     void Update()
     {
-        
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            Debug.Log(hit.collider.gameObject.name);
+        }
     }
 }
